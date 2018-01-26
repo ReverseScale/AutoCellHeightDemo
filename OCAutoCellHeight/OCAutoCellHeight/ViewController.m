8efc012478d50106199f78f8ec9c33281cf635e6
XieTian <Stevens@Stevens-MacBook-Air.local>
2018-01-26 13:23:08 +0800

    修复问题

--- OCAutoCellHeight/OCAutoCellHeight/ViewController.m
+++ OCAutoCellHeight/OCAutoCellHeight/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import <Masonry.h>
+
 #import "AutoMHTableViewCell.h"
 
 @interface ViewController ()<UITableViewDelegate, UITableViewDataSource>
@@ -22,43 +24,59 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    //数据
-    
-    self.arrayList = @[@[@"Logo", @"Workplace"], @[@"Watermark"]];
-
-    self.titleDic = @{
-                       @"Logo":@"GAVTH is a design & development studio.",
-                       @"Workplace":@"Definition of GAVTH in old language is deep pit. The logotype, in fact, composed exclusively of contour and curved lines that inspired from topographic lines. The choice of the color is not random; blue is a represent the figure of depth and clean, gray is an earth surface color and it gives neutrality and quiet.",
-                       @"Watermark":@"Definition of GAVTH in old language is deep pit. The logotype, in fact, composed exclusively of contour and curved lines that inspired from topographic lines."
-                       };
-    self.picDic = @{
-                     @"Logo":@"Icon.png",
-                     @"Workplace":@"PicImg.png",
-                     @"Watermark":@"PicText.png"
-                     };
-
+    [self arrangeData];
     [self tableView];
 }
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Data
+- (void)arrangeData {
+    self.arrayList = @[@[@"Logo", @"Workplace"], @[@"Watermark"]];
+    
+    self.titleDic = @{@"Logo":@"GAVTH is a design & development studio.",
+                      @"Workplace":@"Definition of GAVTH in old language is deep pit. The logotype, in fact, composed exclusively of contour and curved lines that inspired from topographic lines. The choice of the color is not random; blue is a represent the figure of depth and clean, gray is an earth surface color and it gives neutrality and quiet.",
+                      @"Watermark":@"The choice of the color is not random; blue is a represent the figure of depth and clean, gray is an earth surface color and it gives neutrality and quiet."
+                      };
+    self.picDic = @{@"Logo":@"Icon.png",
+                    @"Workplace":@"PicImg.png",
+                    @"Watermark":@"PicText.png"
+                    };
+}
+    
+#pragma mark - Action
 - (void)openDetailView:(NSString *)type {
     [self.navigationController pushViewController:[NSClassFromString(type) new] animated:YES];
 }
+    
+#pragma mark - UITableView DataSource
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     return self.arrayList.count;
 }
-
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     return [[self.arrayList objectAtIndex:section] count];
 }
-
-- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
-    return 0.01;
-}
-- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section {
-    return 0.01;
-}
-
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return [self systemTableView:tableView cellForRowAtIndexPath:indexPath];
+
+//    return [self customCodeTableView:tableView cellForRowAtIndexPath:indexPath];
+}
     
+#pragma mark - View
+/// 系统级
+- (UITableViewCell *)systemTableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:@"systemcell"];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                                      reuseIdentifier:@"systemcell"];
+    }
+    cell.textLabel.text = self.titleDic[self.arrayList[indexPath.section][indexPath.row]];
+    return cell;
+}
+/// 纯代码自定义 Cell
+- (UITableViewCell *)customCodeTableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     static NSString *identifierhot = @"cell";
     AutoMHTableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:identifierhot];
     if (!cell){
@@ -73,31 +91,53 @@
     }
     return cell;
 }
+    
+#pragma mark - UITableView Delegate
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     NSString *titleKey = self.arrayList[indexPath.section][indexPath.row];
     [self openDetailView:titleKey];
 }
 
-//懒加载
+#pragma mark - Lazy
 - (UITableView *)tableView {
     if (!_tableView) {
-        _tableView=[[UITableView alloc]initWithFrame:self.view.bounds style:UITableViewStyleGrouped];
-        _tableView.delegate=self;
-        _tableView.dataSource=self;
-        _tableView.showsVerticalScrollIndicator=NO;
+        _tableView = [[UITableView alloc]initWithFrame:CGRectZero style:UITableViewStylePlain];
+
+        _tableView.delegate = self;
+        _tableView.dataSource = self;
+        _tableView.showsVerticalScrollIndicator = NO;
         _tableView.backgroundColor = [UIColor groupTableViewBackgroundColor];
         [self.view addSubview:_tableView];
         
         _tableView.rowHeight = UITableViewAutomaticDimension;
         _tableView.estimatedRowHeight = 44;
+        
+        //系统注册
+        [_tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"systemcell"];
     }
     return _tableView;
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+#pragma mark - AutoLayout
+- (void)updateViewConstraints {
+    if(@available(iOS 11.0,*)) {
+        self.tableView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentNever;
+    } else {
+        self.automaticallyAdjustsScrollViewInsets = NO;
+    }
+    
+    [self.tableView mas_makeConstraints:^(MASConstraintMaker *make) {
+        if(@available(iOS 11.0,*)) {
+            make.top.equalTo(self.view.mas_safeAreaLayoutGuideTop);
+            make.left.right.equalTo(self.view);
+            make.bottom.equalTo(self.view.mas_safeAreaLayoutGuideBottom);
+        } else {
+            make.size.equalTo(self.view);
+            make.center.equalTo(self.view);
+        }
+    }];
+    
+    [super updateViewConstraints];
 }
-
 
 @end
